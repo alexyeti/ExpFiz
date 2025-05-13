62d1422ff3b04bffc58a0d27afdbd89113edd69e
alexyeti <alexeymalykh2602@gmail.com>
2025-05-14 02:21:54 +0700

    final pls

---
+++ ExpFiz/models/params.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+@dataclass
+class ProjectileParams:
+    """Модель параметров для баллистического эксперимента"""
+    mass: float
+    angle: float
+    gravity: float
+    initial_height: float
+    initial_velocity: float
+    density: float
